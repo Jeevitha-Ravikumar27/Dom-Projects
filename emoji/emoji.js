19f9bce1030059880b4db9620866bbd7c102eb35
Jeevitha-Ravikumar27 <ravikumarjeevitha254@gmail.com>
2024-08-17 15:13:58 +0530

    dom pro

---
+++ emoji/emoji.js
@@ -0,0 +1,20 @@
+const emoji= document.querySelector("#emoji");
+const emojis=[ 
+    "😍",
+    "😂",
+    "😒",
+    "😁",
+    "😃",
+    "😉",
+    "😘",
+    "😇",
+    "😅",
+    "😐",
+    "🙃",
+    "🥰",
+    "😜"
+
+];
+emoji.addEventListener('mouseover',()=>{
+    emoji.innerHTML=emojis[Math.floor(Math.random()*emojis.length)];
+});
